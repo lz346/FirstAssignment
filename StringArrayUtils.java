683e5bf72e4314cff903d093e84bb4859d911603
Linfeng Z <33096195+lz346@users.noreply.github.com>
2020-10-29 17:01:45 -0400

    Finished StringArrayUtil

--- StringArrayUtils.java
+++ StringArrayUtils.java
@@ -9,7 +9,7 @@ public class StringArrayUtils {
      * @return first element of specified array
      */ // TODO
     public static String getFirstElement(String[] array) {
-        return null;
+        return array[0];
     }
 
     /**
@@ -17,7 +17,7 @@ public class StringArrayUtils {
      * @return second element in specified array
      */
     public static String getSecondElement(String[] array) {
-        return null;
+        return array[1];
     }
 
     /**
@@ -25,7 +25,7 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+        return array[array.length-1];
     }
 
     /**
@@ -33,7 +33,7 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+        return array[array.length-2];
     }
 
     /**
@@ -42,6 +42,11 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
+        for (String x : array){
+            if (x == value){
+                return true;
+            }
+        }
         return false;
     }
 
@@ -50,7 +55,15 @@ public class StringArrayUtils {
      * @return an array with identical contents in reverse order
      */ // TODO
     public static String[] reverse(String[] array) {
-        return null;
+        int n = array.length;
+        int j = n;
+        String[] result = new String[n];
+        for (int i = 0; i < n; i++){
+            result[j-1] = array[i];
+            j--;
+        }
+        
+        return result;
     }
 
     /**
@@ -58,7 +71,15 @@ public class StringArrayUtils {
      * @return true if the order of the array is the same backwards and forwards
      */ // TODO
     public static boolean isPalindromic(String[] array) {
-        return false;
+        int i = 0, j = array.length -1;
+        while(i < j){
+            if (array[i] != array[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
 
     /**
@@ -66,8 +87,28 @@ public class StringArrayUtils {
      * @return true if each letter in the alphabet has been used in the array
      */ // TODO
     public static boolean isPangramic(String[] array) {
-        return false;
-    }
+        boolean[] mark = new boolean[26];
+        int index = 0;
+      
+        for (String str : array){
+            for (int i = 0; i < str.length(); i++){
+                if ('A' <= str.charAt(i) && str.charAt(i) <= 'Z') {
+                    index = str.charAt(i) - 'A';
+                    mark[index] = true;
+                }
+                else if ('a' <= str.charAt(i) && str.charAt(i) <= 'z'){
+                    index = str.charAt(i) - 'a';
+                    mark[index] = true;
+                }
+        }}
+        for (boolean x : mark){
+            if (!x){
+                return false;
+            }
+        }
+        
+        return true;
+}
 
     /**
      * @param array array of String objects
@@ -75,7 +116,13 @@ public class StringArrayUtils {
      * @return number of occurrences the specified `value` has occurred
      */ // TODO
     public static int getNumberOfOccurrences(String[] array, String value) {
-        return 0;
+        int count = 0;
+        for (String x : array){
+            if (x == value){
+                count++;
+            }
+        }
+        return count;
     }
 
     /**
@@ -84,7 +131,20 @@ public class StringArrayUtils {
      * @return array with identical contents excluding values of `value`
      */ // TODO
     public static String[] removeValue(String[] array, String valueToRemove) {
-        return null;
+        int count = 0;
+        for (int i=0; i<array.length;i++){
+            if (array[i] == valueToRemove){
+                count++;
+            }
+        }
+        String[] temp = new String[array.length - count];
+        int j = 0;
+        for (int i = 0; i < array.length; i++){
+            if (array[i] != valueToRemove){
+                temp[j++] = array[i];
+            }
+        }
+        return temp;
     }
 
     /**
@@ -92,7 +152,22 @@ public class StringArrayUtils {
      * @return array of Strings with consecutive duplicates removes
      */ // TODO
     public static String[] removeConsecutiveDuplicates(String[] array) {
-        return null;
+        int count = 0;
+        for (int i=0; i<array.length-1;i++){
+            if (array[i] == array[i+1]){
+                count++;
+            }
+        } 
+        String[] temp = new String[array.length - count];
+        int j = 0;
+        for (int i = 0; i < array.length - 1; i++){
+            String current = array[i];
+            if (current != array[i+1]){
+                temp[j++] = current;
+            }
+        }
+        temp[j++] = array[array.length-1];
+        return temp;
     }
 
     /**
@@ -100,7 +175,25 @@ public class StringArrayUtils {
      * @return array of Strings with each consecutive duplicate occurrence concatenated as a single string in an array of Strings
      */ // TODO
     public static String[] packConsecutiveDuplicates(String[] array) {
-        return null;
+        int count = 0;
+        for (int i=0; i<array.length-1;i++){
+            if (array[i] == array[i+1]){
+                count++;
+            }
+        } 
+        String[] temp = new String[array.length - count];
+        int j = 0;
+        temp[0] = array[0];
+        for (int i = 1; i < array.length; i++){
+            if (array[i] == array[i-1]){
+                temp[j] += array[i];
+            }
+            else{
+                temp[++j] = array[i];
+            }
+        }
+                
+        return temp;
     }
 
 
